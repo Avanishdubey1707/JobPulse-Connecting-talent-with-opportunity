900d05c7825b109b601a9cd6f97668b131be48ff
Avanishdubey1707 <avanishdubeymsd@gmail.com>
2026-03-23 21:15:44 +0530

    Header section

--- frontend/job-portal/src/Header/Header.tsx
+++ frontend/job-portal/src/Header/Header.tsx
@@ -1,6 +1,17 @@
+import { IconAsset } from "@tabler/icons-react";
+
 const Header = () =>{
-    return <div>
-        header
+    return <div className="w-full bg-black px-6 text-white h-28 flex justify-between items-center">
+       <div className="flex gap-3">
+           <IconAsset className="h-10 w-10" stroke={1.25}/>
+          Jobpulse
+       </div>
+       <div>
+        links
+       </div>
+       <div>
+        profile
+       </div>
     </div>
 }
 
